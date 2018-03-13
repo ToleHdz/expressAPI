36978cc3cef54aaf396324b5c76b52a9875bf931
AriAlanPR <shaymin2003@gmail.com>
2018-03-13 15:28:14 -0500

    Add Firebase

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var JSONParser = require('body-parser')
 var users = require('./modules/user.js')
 var hardware = require('./modules/hardware.js')
 
-app.use(JSONParser.json()
+app.use(JSONParser.json())
 app.use(JSONParser.urlencoded({ extended: true }))
 app.use(users)
 app.use(hardware)
